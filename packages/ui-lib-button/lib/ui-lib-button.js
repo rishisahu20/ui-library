77dbc7e7fcd82a192ae1b8508f5653ec71272d33
rishisahu20 <rishi.sahu20@gmail.com>
2022-03-14 11:07:28 +0530

    fix

--- packages/ui-lib-button/lib/ui-lib-button.js
+++ packages/ui-lib-button/lib/ui-lib-button.js
@@ -1,12 +1,12 @@
 import React from "react";
-import cx from "clsx";
-import styles from "./styles.css";
+// import cx from "clsx";
+import "./styles.css";
 
 const Button = ({ children, className, variant, ...rest }) => {
   const classes = cx(
     styles.Button,
     {
-      [styles.ButtonSecondary]: variant === "secondary",
+      [styles.ButtonSecondary]: variant === "secondarys",
     },
     className
   );
